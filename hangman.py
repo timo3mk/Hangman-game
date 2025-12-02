f3af3c556ec87c293af6b48542c98eeb157579a9
Ali Bahrmai <a.bahrami.ce@gmail.com>
2025-12-02 21:02:42 +0330

    Version 1 of the Hangman game...
    - Save feature has not been implemented yet.
    + Complete game function implementation.
    + Bugs Fixed.
    (December 2, 2025)

--- hangman.py
+++ hangman.py
@@ -13,11 +13,18 @@ word_bank = [
     "python"
 ]
 
-# note = ''' 
-#     Note:
-#     Wrong guesses draw parts of the hangman.
-#     Finish the word before the figure completes.
-# '''    
+note = '''
+-> Note:
+Your job is simple:
+* Guess the word.
+* Don't let the dude die.
+* Stop typing random nonsense. Thanks.'''
+
+start_game = '''
+Welcome, mighty letter wizard!
+A word is hiding. A stickman is panicking.
+Try not to destroy his day, okay?
+'''
 
 def clear_screan():
     if os.name == 'nt':
@@ -26,7 +33,7 @@ def clear_screan():
         os.system('clear') # Linux Users
         
 def press_enter():
-    input('\nPress Enter to Continue...')
+    input('\nPress Enter to Continue...\n(Yes, THAT Enter. Don\'t overthink it.)')
     
 def word_update(game_word, guess, current_state):
     new_state = list(current_state)
@@ -37,22 +44,24 @@ def word_update(game_word, guess, current_state):
     
     return ''.join(new_state)
 
- 
-
 def menu():
-    run = True
-    while run:
+    while True:
         clear_screan()
+        print('/' * 25)
         print('1. Start Game...')
         print('2. Save Results.')
         print('3. Exit.')
-        print('*' * 50)
+        print('/' * 25)
         
         try:
             choice = int(input('Enter number of the option(1, 2, 3) '))
             if choice <= 3 and choice >=1:
                 if choice == 1:
                     game(choice)
+                elif choice == 2:
+                    pass
+                else:
+                    break
                 
             else:
                 print('Enter number between 1 to 3...')
@@ -62,17 +71,23 @@ def menu():
 
 def game(choice):
     clear_screan()
+    print(start_game)
+    press_enter()
+    clear_screan()
     game_logic = lg.logic()
     game_word = random.choice(word_bank).lower()
     count = 0 
+
+    print(lg.hangman_ascii[0]) # Print hangman[0]
+    print(note)
+    press_enter()
+    clear_screan()
     
     # Print _ _ _ _
     current_state = '_' * len(game_word) 
     word = [current_state]
     print(' '.join(current_state))
     
-    print(lg.hangman_ascii[0]) # Print hangman[0]
-         
     while True:
         guess = input('\nEnter Your Guess(a-z) : ').lower().strip()
         
@@ -81,10 +96,10 @@ def game(choice):
             
             if result == False:
                 count = attempt
-                print('Nope...')
+                print('-> Wrong guess!\nStickman took emotional damage.')
                 
             else:
-                print('Good guess :) ')
+                print('-> Nice! You actually guessed something right!\nStickman is relieved... for now.')
             
             if count != 0:
                 print(hangman)
@@ -94,16 +109,17 @@ def game(choice):
             if word[0] == game_word:
                 clear_screan()
                 print('Word : ', word[0])
-                print('You Won...')
+                print('-> YOU WON!\nStickman lives another day!\nHe says “thanks” (or he would, if he had a mouth)')
                 press_enter()
                 break
                          
         else:
-            print('Enter the letter not word or something else...')
+            print('-> WOW. Incredible.\nYou typed something that is not a letter at all.\nStickman is judging you silently.')
             count += 1
+            print(lg.hangman_ascii[count])
             
         if count == 6:
-            print('You do not have any chance.')
+            print('-> Welp… he\'s dead now.\nHope you\'re proud of yourself')
             print('TRY AGAIN...')
             press_enter()
             break
@@ -113,4 +129,4 @@ def game(choice):
         print(' '.join(word[0]))
     
 if __name__ == '__main__':
-    game(3)    
+    menu()    
